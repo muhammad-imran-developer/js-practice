e335bc73d1ecd0801ddf84e52cfa54b2c42b3607
muhammad-imran-developer <167766582+muhammad-imran-developer@users.noreply.github.com>
2025-10-31 22:54:02 +0500

    Dates in js
    Dates in js

---
+++ date.js
@@ -0,0 +1,11 @@
+const myDate = new Date();
+console.log(myDate) /
+console.log(myDate.toString()) 
+console.log(myDate.toLocaleString()) 
+console.log(myDate.toLocaleDateString()) 
+console.log(myDate.toLocaleTimeString()) 
+console.log(myDate.toDateString()) 
+console.log(myDate.toTimeString()) 
+console.log(myDate.toISOString()) 
+console.log(myDate.toJSON()) 
+
